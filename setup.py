178a57ec303472b876eb17700adb2526430b73dc
carlosdeantonio <charliedeantonio@gmail.com>
2019-07-15 19:43:23 +0200

    fixed and ready to release v1.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as file:
 
 setup(
     name='crypton',
-    version='1.0.0',
+    version='1.0.1',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/OverwatchHeir/CryptOn.git',
